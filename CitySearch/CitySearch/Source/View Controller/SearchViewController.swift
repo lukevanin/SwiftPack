cb8e866575b53dca5c11b3d0467fe4ddd03ed21b
Luke Van In <lukevanin@gmail.com>
2022-06-09 16:30:18 +0200

    Add CitySearchModelProtocol
    - Update CitySearchModel to conform to CitySearchModelProtocol
    - Add SearchViewController
    - Add unit tests for SearchViewController

---
+++ CitySearch/CitySearch/Source/View Controller/SearchViewController.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+final class SearchViewController: UICollectionViewController {
+    
+    private let model: CitySearchModel
+    
+    init(model: CitySearchModel) {
+        self.model = model
+        let layoutGroup = NSCollectionLayoutGroup(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .absolute(300)
+            )
+        )
+        let layoutSection = NSCollectionLayoutSection(
+            group: layoutGroup
+        )
+        let layout = UICollectionViewCompositionalLayout(
+            section: layoutSection
+        )
+        super.init(collectionViewLayout: layout)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("This view controller cannot be used from a Storyboard")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemGreen
+    }
+
+
+}
+
---
+++ CitySearch/CitySearchTests/SearchViewControllerTests.swift
@@ -0,0 +1,59 @@
+import XCTest
+
+@testable import CitySearch
+
+final class SearchViewControllerTests: XCTestCase {
+    
+    private var mockModel: MockCitySearchModel!
+    private var subject: SearchViewController!
+    
+    override func setUp() {
+        mockCitiesRepository = MockCitiesRepository()
+        model = CitySearchModel(citiesRepository: mockCitiesRepository)
+        subject = SearchViewController(model: model)
+    }
+    
+    override func tearDown() {
+        subject = nil
+    }
+    
+    func testCollectionView_shouldShowNothing_givenNoCities() {
+        subject.viewDidLoad()
+        subject.viewWillAppear(false)
+        subject.viewDidAppear(false)
+    }
+
+    func testCollectionView_shouldShowAllCities_givenSomeCities() {
+        subject.viewDidLoad()
+        subject.viewWillAppear(false)
+        subject.viewDidAppear(false)
+    }
+
+    func testCollectionView_shouldUpdate_whenModelPublishesChangesAfterViewWillAppear() {
+        subject.viewDidLoad()
+        subject.viewWillAppear(false)
+    }
+
+    func testCollectionView_shouldNotUpdate_whenModelPublishesChangesAfterViewWillDisappear() {
+        subject.viewDidLoad()
+        subject.viewWillAppear(false)
+        subject.viewDidAppear(false)
+        subject.viewWillDisappear(false)
+    }
+
+    func testSearchInput_shouldShowNothing_givenNoCities() {
+        
+    }
+
+    func testSearchInput_shouldShowNothing_givenNoMatchingCities() {
+        
+    }
+
+    func testSearchInput_shouldShowCities_givenMatchingCities() {
+        
+    }
+    
+    func testClearSearch_shouldShowAllCities_givenSomeCities() {
+        
+    }
+}
